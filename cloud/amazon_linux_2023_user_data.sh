84aca3c2b0aa2c72d021f780d774befa0e9b2e8b
Victor PS <105178616+vps-victor@users.noreply.github.com>
2023-08-13 14:29:31 -0300

    Create amazon_linux_2023_user_data.sh

---
+++ cloud/amazon_linux_2023_user_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+wget https://aws-tc-largeobjects.s3-us-west-2.amazonaws.com/DEV-AWS-MO-GCNv2/FlaskApp.zip
+unzip FlaskApp.zip
+cd FlaskApp/
+yum -y install python3-pip
+pip install -r requirements.txt
+yum -y install stress
+export PHOTOS_BUCKET=${SUB_PHOTOS_BUCKET}
+export AWS_DEFAULT_REGION=<INSERT REGION HERE>
+export DYNAMO_MODE=on
+FLASK_APP=application.py /usr/local/bin/flask run --host=0.0.0.0 --port=80 
