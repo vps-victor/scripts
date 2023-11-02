5fbd59087ef712eb956976d7351aea00d353d733
Victor PS <105178616+vps-victor@users.noreply.github.com>
2023-11-02 20:31:18 -0300

    Update CLI scripts

---
+++ helper/update.sh
@@ -0,0 +1,1 @@
+sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
---
+++ helper/updown.sh
@@ -0,0 +1,1 @@
+sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y&& shutdown -h now
